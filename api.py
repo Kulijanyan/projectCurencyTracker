936390f3e3e1cf89c8d61d24be7739401caf571f
Kulijanyan Georgi <kulgeos@gmai.com>
2021-05-12 12:54:44 +0400

    first version done!

--- api.py
+++ api.py
@@ -4,6 +4,13 @@ import requests
 def rate_getter():
     url = "http://api.exchangeratesapi.io/v1/latest?access_key=14ac6512ce2db0e6355ee053f6ad8827&base=EUR&symbols=USD,AMD,RUB"
     response = requests.request("GET", url)
-    rates = response.json()["rates"]
-    rates["EUR"] = 1
-    return rates
+    if not response.json()["success"]:
+        raise ConnectionError(f'Something went wrong :( Success: {response.json()["success"]}')
+    else:
+        rates = response.json()["rates"]
+        rates["EUR"] = 1
+        return rates
+
+
+if __name__ == "__main__":
+    rate_getter()
--- main.py
+++ main.py
@@ -8,6 +8,8 @@ class ElementFunctions(QtWidgets.QMainWindow):
         super(ElementFunctions, self).__init__()
         self.ui = Ui_CurrencyTracker()
         self.ui.setupUi(self)
+        self.ui.exchange_value1.setPlaceholderText("0")
+        self.ui.exchange_value2.setPlaceholderText("0")
         self.functions()
 
     def functions(self):
@@ -31,10 +33,37 @@ class ElementFunctions(QtWidgets.QMainWindow):
                 self.ui.rub_valuetxt.setText(str(1))
                 self.ui.amd_valuetxt.setText(str(round(rates["AMD"] / rates["RUB"], 2)))
 
-        # def exchanger():
-        #     print(self.ui.exchange_value1.toPlainText())
-        #
-        # exchanger()
+        self.ui.exchange_button.clicked.connect(lambda: exchanger())
+
+        def exchanger():
+            if self.ui.base_value_2.currentText() == "---" or self.ui.base_value_3.currentText() == "---":
+                self.ui.curr_error.exec_()
+                self.ui.base_value_2.setCurrentIndex(1)
+                self.ui.base_value_3.setCurrentIndex(2)
+            else:
+                curr_1 = self.ui.base_value_2.currentText()
+                curr_2 = self.ui.base_value_3.currentText()
+
+                first_val = self.ui.exchange_value1.text()
+                second_val = self.ui.exchange_value2.text()
+
+                if self.ui.exchange_value2.isModified():
+                    if second_val.isnumeric():
+                        res = float(second_val) / (rates[curr_2] / rates[curr_1])
+                        res = str(round(res, 2))
+                        self.ui.exchange_value1.setText(res)
+                        self.ui.exchange_value1.setModified(False)
+                        self.ui.exchange_value2.setModified(False)
+                    else:
+                        self.ui.value_error.exec_()
+                elif self.ui.exchange_value1.isModified():
+                    if first_val.isnumeric():
+                        res = float(first_val) * (rates[curr_2] / rates[curr_1])
+                        res = str(round(res, 2))
+                        self.ui.exchange_value2.setText(res)
+                        self.ui.exchange_value2.setModified(False)
+                    else:
+                        self.ui.value_error.exec_()
 
 
 if __name__ == "__main__":
